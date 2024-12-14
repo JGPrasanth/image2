8e1df225bc3986d2d1adad2eff978d5f7606883f
J G PRASANTH <144326060+JGPrasanth@users.noreply.github.com>
2024-12-14 11:08:14 +0530

    Add files via upload

---
+++ Trap_Music (1)/Trap_Music/src/main/java/com/example/Trap_Music/TrapMusicApplication.java
@@ -0,0 +1,13 @@
+package com.example.Trap_Music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrapMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrapMusicApplication.class, args);
+	}
+
+}
---
+++ Trap_Music (1)/Trap_Music/src/test/java/com/example/Trap_Music/TrapMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Trap_Music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrapMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
